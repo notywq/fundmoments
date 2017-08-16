e10d618bf7fb891b553cba0498e243f2566bd35f
Baron Dimaranan <barondimaranan@gmail.com>
2017-08-16 15:38:55 +0800

    ugh run . py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 #!flask/bin/python
 import os
 from app import app
-port = int(os.environ.get('PORT', 5000)) app.run(host='0.0.0.0', port=port)
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
